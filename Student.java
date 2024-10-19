52c675289cf632179ce43cf21510a3dd7fba106a
bllavanya29 <bllavanya29@gmail.com>
2024-10-19 14:03:47 -0400

    Add files via upload

---
+++ Student.java
@@ -0,0 +1,27 @@
+class Student
+{
+	public static void main(String[] args)
+	{
+		/*This is 
+		an example
+		of multiple lines
+		comments
+		*/
+		//data_type variable_name = variable_value;
+		int rollNo = 109;
+		String studentName = "John Smith";
+		int age = 18;
+		double marks = 87.987;
+		char gender = 'M';
+		boolean scolarship = false;
+		
+		System.out.println("Student Details are as follows: ");
+		System.out.println("Student Roll Number is: " + rollNo);//operator + is concatination operator.
+		System.out.println("Student Name is: " + studentName);
+		System.out.println("Student Age is: " + age);
+		System.out.println("Student Marks are: " + marks);
+		System.out.println("Student Gender is: " + gender);
+		System.out.println("Does Student have scolarship?: " + scolarship);
+		//Student Roll Number is: 109
+	}
+}
