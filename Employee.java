aeb9f7b6b8cef75936d92ce2b27d1159de390ce5
bllavanya29 <bllavanya29@gmail.com>
2024-10-19 14:03:02 -0400

    Add files via upload

---
+++ Employee.java
@@ -0,0 +1,19 @@
+class Employee
+{
+	public static void main(String[] args){
+		int employeeId = 101;
+		String employeeName = "Yash Gupta";
+		double salary = 199425.35;
+		double employeeExperience = 2.5;
+		boolean employeePermanent = true;
+/*
+		System.out.println("Employee Id is: "+ employeeId + ".");
+		System.out.println("Employee Name is: "+ employeeName);
+		System.out.println("Employee salary is: "+ salary );
+		System.out.println("Employee experiance is: "+ employeeExperience + " years." );
+		System.out.println("Employee permanent: "+ employeePermanent );
+		*/
+		System.out.println("Employee details are: " + "Emp Id is: " + employeeId + "  Emp Name is: " + employeeName + " Emp Salary is: " + salary + "  Emp Exp. is: " + employeeExperience + "  Emp Status is: " + employeePermanent);
+	}
+
+}
